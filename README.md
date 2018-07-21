879fd0e968a8931b41c5ecb70895117e9be27fbb
Ronald <ronaldceballos15@gmail.com>
2018-07-21 15:53:50 -0400

    Fixes and suggestions from Udacity review have been applied, code run faster, better format

--- README.md
+++ README.md
@@ -4,16 +4,32 @@ This python code connects with a database and adquires useful information about 
 ## Install
  - Download python from [here](https://www.python.org/)
  - Fork and clone this repository [here](https://github.com/Ronald03/log_analysis)
-  
  
-## How to use
-- To read or modify the code, go to the directory where you cloned or downloaded the code
-- Use python IDLE or a text editor (i.e. SublimeText 3) to inspect and modify the code
-- To see the result of the code run the log_analysis.py file by pressing (F5) from the Python IDLE or on a vagrant maching running the required database and running the command 'python log_analysis.py' on the directory containing file 
-- The program will create a .txt file on the same directory where the program file is located
+## Requirements
+- Download and install [Vagrant](https://www.vagrantup.com/downloads.html)
+- Download and install [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+- Download this pre-configure vagrant environment from [here](https://s3.amazonaws.com/video.udacity-data.com/topher/2017/June/5948287e_fsnd-virtual-machine/fsnd-virtual-machine.zip)
+
+## Set environment
+- Unzip the pre-configure vagrant environment on a desired directory
+- From a terminal navigate the unziped directory and locate the `Vagrantfile` file under the `Vagrant` folder
+- Download the database from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+-  Place database on the `vagrant` folder; this is a common folder beteween the Virtual machine and the host machine
+- Bring the virtual machine back online (with `vagrant up`)
+- Then log into it with `vagrant ssh`.
+- To load the data, `cd` into the `vagrant` directory and use the command `psql -d news -f newsdata.sql`
+- Finally place the `log_analysis.py` from this project on the `Vagrant` directory
+
+## How to run
+Once the Database has been loaded on the virtual machine, if not logged on already, log to the virtual mechine from a terminal using the command `vagrant ssh`. 
+
+Navigate to the `Vagrant` directory by typing `cd /vagrant` and from here execute the python file by typing `python log_analysis.py`.
+
+Once the machine completes the execution, look for the `art_report.txt` file on the `vagrant` directory containing the final result of the report.
 
 ### Notes
-> No VIEWS are needed to run this code; every result has been accomplished with one query 
+> If you need or want to modify the source file and result of this project, just open the `log-analysis.py` file from a text editor and modify it as you need.
+> No VIEWS were required to get the information from the database; every result has been accomplished with one query 
 
 ## License
 This is .....**free to the public!**
--- log_analysis.py
+++ log_analysis.py
@@ -1,83 +1,113 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
-
-DBNAME = "news"
-
-try:
-    db = psycopg2.connect(database=DBNAME)
-except:
-    print("Unable to connect to the database")
-
-# Create a connection to the data base
-connect = db.cursor()
-
-# Most popular three articles of all time;
-# Which articles have been accessed the most?
+# A function to connect to the DB, it will be invoked on every other function
 
 
-def popular_authors(file):
+def db_connect():
+    DBNAME = "news"
+    try:
+        db = psycopg2.connect(database=DBNAME)
+        conn = db.cursor()
 
-    # Use the global openned connection
-    connect.execute("SELECT articles.title, COUNT(log.path) AS views FROM log "
-                    "JOIN articles ON log.path like '%' || articles.slug || ''"
-                    " GROUP BY articles.title ORDER BY views DESC limit 3;")
+        # Return tuple, DB connection and cursor
+        return db, conn
+    except:
+        # If not able to connect throws error
+        print("Unable to connect to the database")
 
-# This will hold the result from the query converted from tuple to array (this"
-# will be a 2 dimensional array)
-    result_arr = []
 
-# This variable will hold the formatted data and ready to be added to the
-# output file
+def popular_articles():
+    """
+     This method will return a formatted string listing the
+     most porpular articles based on their view amount
+    """
 
-    file.write("\t         The most viewed articles are: \n")
+    # Create a connection with DB
+    db, connect = db_connect()
 
-# save result from query by fetchinig it to the result variable
+    connect.execute("""
+                    SELECT articles.title, COUNT(log.path) AS views FROM log
+                    JOIN articles ON log.path like '%' || articles.slug || ''
+                    GROUP BY articles.title ORDER BY views DESC limit 3;
+                    """)
+
+    # Convert fetched result into a list
     result = list(connect.fetchall())
 
+    output = "\t         The most viewed articles are: \n"
+
     for i in result:
-        # Convert result of the query from tuple to list/array and add them to
-        # result_arr; not really in use
-        result_arr.append(list(i))
+        output += "\n \"" + i[0] + "\" -- " + str(i[1]) + " views \n"
 
-# Extract title and views and add them to a variable; it is already formatted
-# with spaces and new lines:
-
-        file.write("\n " + i[0] + " -- " + str(i[1]) + " views \n \n")
-
-    popular_articles(file)
+    # Close connection with DB
+    db.close()
+    return output
 
 # Most popular article authors of all time;
 # which authors get the most page views.
 
 
-def popular_articles(file):
-    connect.execute("SELECT authors.name, COUNT(authors.name) AS author_count,"
-                    " authors.id FROM authors JOIN articles ON authors.id = "
-                    "articles.author JOIN log ON log.path LIKE '%' ||"
-                    " articles.slug || '' GROUP BY authors.name, authors.id "
-                    "ORDER BY authors.id;")
-    file.write("\t       The most popular article authors: \n")
-    result = list(connect.fetchall())
-    for j in result:
-        file.write("\n " + j[0] + " -- " + str(j[1]) + " views \n \n")
+def popular_authors():
+    """
+    This method returns a formatted string listing the popularity
+    of authors based on their article's views
+    """
+    # Create a connection with DB
+    db, connect = db_connect()
 
-    errors_count(file)
+    connect.execute("""
+                    SELECT authors.name, COUNT(authors.name) AS author_count,
+                    authors.id FROM authors JOIN articles ON authors.id =
+                    articles.author JOIN log ON log.path = '/article/' ||
+                    articles.slug || '' GROUP BY authors.name, authors.id
+                    ORDER BY authors.id;
+                    """)
+
+    result = list(connect.fetchall())
+    output = "\n \t       The most popular article authors: \n"
+    for j in result:
+        output += "\n " + j[0] + " -- " + str(j[1]) + " views \n"
+
+    # Close connection with DB
+    db.close()
+    return output
 
 # On which days did more than 1% of requests lead to errors?
 
 
-def errors_count(file):
+def errors_count():
+    """
+    This method returns all possible dates where the margin or error requests
+    are were more than 1%.
+    """
 
-    connect.execute("SELECT TO_CHAR(time::DATE, 'Mon DD, YYYY'), to_char(ROUND"
-                    "(COUNT(status) FILTER (WHERE status != '200 OK')/CAST"
-                    "(COUNT(status) AS DECIMAL), 2) * 100, '9.9') AS PERCNT "
-                    "FROM log GROUP BY time::DATE ORDER BY PERCNT DESC;")
-    file.write("\n       Days with more than 1% of Error: \n")
+    # Create a connection with DB
+    db, connect = db_connect()
+
+    # Execute query against the DATABASE
+    connect.execute("""
+                    SELECT TO_CHAR(time::DATE, 'Mon DD, YYYY'), to_char(ROUND
+                    (COUNT(status) FILTER (WHERE status != '200 OK')/CAST
+                    (COUNT(status) AS DECIMAL) * 100, 2), '9.9') AS PERCNT
+                    FROM log GROUP BY time::DATE ORDER BY PERCNT DESC;
+                    """)
+
+    # Create output variable to be return with final output of the function
+    output = "\n \t       Days with more than 1% of Error: \n"
+
+    # Fetch the result from the query
     result = connect.fetchall()
+
+    # Extract and format result into the output variable
     for x in result:
         if float(x[1]) > 1:
-            file.write("\n - " + x[0] + " -- " + x[1] + "%. \n")
-            file.write("\n")
+            output += "\n - " + x[0] + " -- " + x[1] + "%. \n"
+
+    # Close connection with DB
+    db.close
+    return output
 
 
 def main():
@@ -85,12 +115,14 @@ def main():
     # Create file
     out_file = open('art_report.txt', 'a')
 
-    # Pass file to popular_authors variable; all other functions
-    # will be called from whitin each function
-    popular_authors(out_file)
+    # Write the returned output from each function into the file
+    out_file.write(popular_articles())
+    out_file.write(popular_authors())
+    out_file.write(errors_count())
 
     # Close file
     out_file.close()
 
 # execute main function to start the report's creation
-main()
+if __name__ == '__main__':
+    main()
